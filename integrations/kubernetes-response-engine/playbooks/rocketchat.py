bb52a6521247ef8ef1e4f2cba7c7395c4fb22423
Alexander Kalinovski <a.kalinovski@gmail.com>
2018-11-07 16:38:27 +0300

    Update rocketchat.py

--- integrations/kubernetes-response-engine/playbooks/rocketchat.py
+++ integrations/kubernetes-response-engine/playbooks/rocketchat.py
@@ -4,9 +4,11 @@ sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__))))
 import os
 import requests
 import json
+import playbooks
+from playbooks import infrastructure
 
-playbook = AddMessageToRocketChat(
-    RocketChatClient("http://rocketchat-rocketchat.cicd-tools:3000")
+playbook = playbooks.AddMessageToRocketChat(
+    infrastructure.RocketChatClient("http://rocketchat-rocketchat.cicd-tools:3000")
 )
 
 def handler(event, context):
