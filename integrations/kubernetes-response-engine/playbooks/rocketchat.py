fe99ef1030dd5cf5c573276df4cc1c254eefa38e
Alexander Kalinovski <a.kalinovski@gmail.com>
2018-11-07 17:46:47 +0300

    Create rocketchat.py

---
+++ integrations/kubernetes-response-engine/playbooks/rocketchat.py
@@ -0,0 +1,12 @@
+import sys
+import os.path
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__))))
+import playbooks
+from playbooks import infrastructure
+
+playbook = playbooks.AddMessageToRocketChat(
+    infrastructure.RocketChatClient("http://rocketchat-rocketchat.cicd-tools:3000")
+)
+
+def handler(event, context):
+    playbook.run(event['data'])
